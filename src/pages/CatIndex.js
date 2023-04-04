d4e6adbc0e91eb17bf763876306f8feed99e1658
Elyse Montano <elysemontano.dev.com>
2023-04-04 16:00:56 -0700

    adds folder structure, files, routes, reactstrap, and styling for header, footer, and notfound

---
+++ src/pages/CatIndex.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CatIndex = () => {
+  return (
+    <>
+      <h1>Here is all of my cats!</h1>
+      <p>Aren't they cute????</p>
+    </>
+  )
+}
+
+export default CatIndex
